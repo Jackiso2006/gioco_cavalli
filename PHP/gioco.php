8baa447652e2b8cebdcb0ff312fb4f28c5dc8aa0
Jackiso2006 <isocomo06@gmail.com>
2024-01-24 17:05:04 +0100

    stampo carte

--- PHP/gioco.php
+++ PHP/gioco.php
@@ -12,23 +12,30 @@
 <body>
     <?php
     $scelta = "";
-    if (isset($_POST['scelta_c'])) {
+    if (isset($_POST['scelta_c']))
         $scelta="c";
-    } else if (isset($_POST['scelta_d'])) {
+    else if (isset($_POST['scelta_d'])) 
         $scelta="d";
-    } else if (isset($_POST['scelta_h'])) {
+    else if (isset($_POST['scelta_h'])) 
         $scelta="h";
-    } else if (isset($_POST['scelta_s'])) {
+    else if (isset($_POST['scelta_s'])) 
         $scelta="s";
-    }
+    else if(isset($_POST['seme']))
+        $scelta=$_POST['seme'];
+    else
+        echo "Qualcosa è andato storto";
+    
 
     $carte = explode(',', $_POST["carte"]);
     $posizioni = explode(',', $_POST["posizioni"]);
 
 
-    // tuttiMaggiori($num){
-    //     // se tutte le posizioni sono maggiori di 
-    // }
+    function tuttiMaggiori($arr, $num){
+        foreach ($arr as $pos) 
+            if ($pos <= $num) 
+                return false;
+        return true;
+    }
     
     ?>
 
@@ -37,19 +44,32 @@
         for($i=0; $i<5;$i++){
             echo "<div>";
             for($j=0; $j<6; $j++){
-                /* if c'è la carta in questa posizione -> stampo la carta (switch tra i semi) */
-                /* else stampo div vuoto (occuperà spazio perchè glielo dico col css) */
                 if($i == 0){
-                    // if(tuttiMaggiori(6-$j))
-                    echo "<div class='card'><img src='../IMG/dorso.JPG'></div>";
-
+                    if(!tuttiMaggiori($posizioni , $j)) // controllo per girare le carte a sinistra
+                        echo "<div class='card'><img src='../IMG/dorso.JPG'></div>";
+                    // else ...
                 }
-                else
-                    echo "<div class='card'>carta</div>";
+                else{
+                    if($posizioni[$i - 1]== 5 - $j)
+                        switch($i){
+                            case 1: echo "<div class='card'><img src='../IMG/bg_c13.gif'></div>"; break;
+                            case 2: echo "<div class='card'><img src='../IMG/bg_d13.gif'></div>"; break;
+                            case 3: echo "<div class='card'><img src='../IMG/bg_h13.gif'></div>"; break;
+                            case 4: echo "<div class='card'><img src='../IMG/bg_s13.gif'></div>"; break;
+                        }
+                    else
+                        echo "<div class='card'></div>";
+                }
             }
             echo "</div>";
         }
-        // qua stampo il mazzo
+        // stampo il mazzo
+        echo "<form action='./gioco.php' method='post'>
+                <button name='scelta_c'><img src='../IMG/dorso.JPG'></button>
+                <input type='hidden' id='carte' name='carte' value=". implode(", ", $carte) .">
+                <input type='hidden' id='posizioni' name='posizioni' value=". implode(", ", $posizioni) .">
+                <input type='hidden' id='seme' name='seme' value=". $scelta .">
+            </form>";
     ?>
    </main> 
 
